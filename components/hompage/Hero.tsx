ebbff2109bf0e23afa963f8930d5230d0c76b421
Steven Cabugos <stevencabugos138@gmail.com>
2025-06-20 08:46:59 +0800

    feat: add correct header styles

--- components/hompage/Hero.tsx
+++ components/hompage/Hero.tsx
@@ -3,8 +3,10 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="section-hero pt-[160px]">
-      <h1 className="text-4xl md:text-7xl lg:text-8xl text-center lg:text-start font-nm-medium font-medium text-white w-auto lg:w-[53%] lg:leading-[77px]">
-        Design Without Limits
+      <h1 className="text-4xl md:text-7xl lg:text-8xl text-center md:text-center lg:text-start font-nm-medium font-medium text-white w-auto lg:leading-[77px]">
+        Design 
+        <br />
+        Without Limits
       </h1>
       <p className="text-base md:text-2xl text-center lg:text-start font-nm-book text-white w-auto lg:w-[53%] mt-4">
         Designing and Developing Unique Web Experiences
