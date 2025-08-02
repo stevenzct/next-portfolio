237dc25af309d730d8142af9179d9239b33d621a
stevenzct <stevencabugos138@gmail.com>
2025-08-02 08:41:25 +0800

    feat: add hover colors for button

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,11 +7,11 @@ type ButtonProps = {
 const Button = ({ type, title, onClick }: ButtonProps) => {
   return (
     <button
-      className={`w-48 h-12 rounded-lg bg-white inline-flex justify-center items-center px-5 py-2.5 cursor-pointer`}
+      className={`w-48 h-12 text-black rounded-lg bg-white inline-flex justify-center items-center px-5 py-2.5 cursor-pointer hover:bg-black hover:text-white transition-colors duration-300`}
       type={type}
       onClick={onClick}
     >
-      <label className="text-black font-nm-medium font-medium text-base cursor-pointer">
+      <label className=" font-nm-medium font-medium text-base cursor-pointer">
         {title}
       </label>
     </button>
