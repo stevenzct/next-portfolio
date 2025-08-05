e451496b424d6bfc843470d3cb9fb5bf253dfba9
stevenzct <stevencabugos138@gmail.com>
2025-08-05 18:54:16 +0800

    feat: add dot in description

--- constants/projectDetails.ts
+++ constants/projectDetails.ts
@@ -17,13 +17,13 @@ export type ProjectDetails = {
 export const projectDetails: ProjectDetails[] = [
     {
         title: "RV Rioflorido",
-        description: "RV Rioflorido is a civil engineer-led construction company in the Philippines, specializing in residential and commercial projects with modern finishes and comprehensive services",
+        description: "RV Rioflorido is a civil engineer-led construction company in the Philippines, specializing in residential and commercial projects with modern finishes and comprehensive services.",
         role: "Designer, Developer",
         tech: "Figma, Vue.js, Nuxt.js, Tailwind",
         link: "rvriofloridocon.com",
         year: 2025,
         imageSrcMockup: "/images/projectDetails/Rioflorido/RiofloridoMockup.jpg",
-        assignment: "Designed and developed a clean, modern website for RVRioFlorido Construction to showcase their projects and services. The site delivers a professional, minimalist experience that reflects the company’s credibility and expertise in residential and commercial construction",
+        assignment: "Designed and developed a clean, modern website for RVRioFlorido Construction to showcase their projects and services. The site delivers a professional, minimalist experience that reflects the company’s credibility and expertise in residential and commercial construction.",
         objectives: "To build a digital presence that reflects RVRioFlorido Construction’s professionalism combining clean design with solid frontend architecture for a seamless, trustworthy user experience across all devices.",
         projectIncludes: [
             "Design Ideation",
