2baaae0cfe5663fc12675b75a3e381e3bfacd3c2
stevenzct <stevencabugos138@gmail.com>
2026-07-16 12:29:05 +0800

    chore: add eyebrow headline

--- components/hompage/About.tsx
+++ components/hompage/About.tsx
@@ -144,6 +144,9 @@ const About = () => {
       <div className="container-wrapper w-full h-auto">
         <div className="app-container mx-6 w-auto max-w-[1200px] md:mx-12 lg:mx-auto lg:w-[90%] xl:w-[88%] 2xl:w-[75%]">
           <div className="mb-8 md:mb-10 lg:mb-16">
+            <p className="mb-2 font-nm-book text-base md:text-xl lg:text-2xl">
+              My Expertise
+            </p>
             <h2 className="w-auto text-start font-nm-medium text-[32px] font-medium leading-[30px] text-black md:text-6xl md:leading-[0.95] lg:text-7xl xl:text-8xl xl:leading-[77px]">
               About Me
             </h2>
